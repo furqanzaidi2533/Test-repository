74970e0510bd170da702ae9ee4a15ee6b2aa515f
Ahsanwarsi <ahsan.ue@gmail.com>
2024-02-21 17:42:26 +0500

    add try code file

--- app/src/main/java/com/example/firstdemoproject/TryCode.java
+++ app/src/main/java/com/example/firstdemoproject/TryCode.java
@@ -1,2 +1,11 @@
-package com.example.firstdemoproject;public class TryCode {
+package com.example.firstdemoproject;
+
+import android.app.Dialog;
+import android.content.Context;
+
+public class TryCode {
+    public static void showDialog(Context context){
+        Dialog dialog= new Dialog(context);
+        dialog.show();
+    }
 }
